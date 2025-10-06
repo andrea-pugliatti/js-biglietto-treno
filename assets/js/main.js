19f1955e254005706cc3a851ebab1a042cd271eb
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-06 13:53:20 +0200

    Add tools and subtasks

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*
+    TOOLS:
+    prompt,
+    if else,
+    arithmetic operators,
+    toFixed()
+*/
+
+// Chiedi all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+
+// Calcola il biglietto
+
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// Va applicato uno sconto del 20% per i minorenni
+// Va applicato uno sconto del 40% per gli over 65
+
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
