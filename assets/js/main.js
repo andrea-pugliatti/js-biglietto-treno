c41f5a2bb83b7499b21170bfe51b3a3f2ae12550
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-06 14:07:23 +0200

    Calculate the price of the ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,15 @@ const age = Number(prompt("Qual è l'età del passeggero?"));
 console.log(distance, age);
 
 // Calcola il biglietto
-
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let cost = distance * 0.21;
 // Va applicato uno sconto del 20% per i minorenni
+if (age <= 18) {
+	cost -= cost * 0.2;
+}
 // Va applicato uno sconto del 40% per gli over 65
+if (age >= 65) {
+	cost -= cost * 0.4;
+}
 
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
